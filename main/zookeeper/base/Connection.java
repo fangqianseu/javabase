6c98d98ae73b8e4883ca1738cf12763e45a83fe2
fangqianseu <fangqiandndx@gmail.com>
2019-05-08 21:57:24 +0800

    zookeeper入门： 连接、创建节点

---
+++ main/zookeeper/base/Connection.java
@@ -0,0 +1,47 @@
+/*
+Date: 05/08,2019, 21:18
+
+zookeeper连接操作
+*/
+package zookeeper.base;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Connection {
+    private static final String host = "120.78.193.198";
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+
+    public static void main(String[] args) throws Exception {
+        // 异步连接 需要等待
+        ZooKeeper zookeeper = new ZooKeeper(host, 5000, new SimpleWatcher());
+        System.out.println(zookeeper.getState());
+        try {
+            connectedSemaphore.await();
+        } catch (InterruptedException e) {
+        }
+        System.out.println("ZooKeeper session first established.");
+
+        // 使用上一次连接的凭证
+        long sessionId = zookeeper.getSessionId();
+        byte[] passwd = zookeeper.getSessionPasswd();
+        System.out.println("sessionID: " + sessionId + "， passed:" + new String(passwd));
+
+        zookeeper = new ZooKeeper(host, 5000, new SimpleWatcher(), sessionId, passwd);
+
+        System.out.println("ZooKeeper session second established.");
+        Thread.sleep(1000);
+    }
+
+    static class SimpleWatcher implements Watcher {
+        public void process(WatchedEvent event) {
+            System.out.println("Receive watched event：" + event);
+            if (Watcher.Event.KeeperState.SyncConnected == event.getState()) {
+                connectedSemaphore.countDown();
+            }
+        }
+    }
+}
---
+++ main/zookeeper/base/CreateNode.java
@@ -0,0 +1,56 @@
+/*
+Date: 05/08,2019, 21:32
+*/
+package zookeeper.base;
+
+import org.apache.zookeeper.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+
+import static org.apache.zookeeper.Watcher.Event.KeeperState;
+
+public class CreateNode {
+    private static final String host = "120.78.193.198";
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+    private static CountDownLatch createNodeSemaphore = new CountDownLatch(1);
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        ZooKeeper zookeeper = new ZooKeeper(host, 5000, new SimpleWatcher());
+        connectedSemaphore.await();
+
+        // 同步方式创建节点
+        System.out.println("Create Sync");
+        String res = zookeeper.create("/zk-test-Sync", "Sync".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        System.out.println("Success create znode: " + res);
+
+        // 异步方式创建节点
+        System.out.println("Create ASync");
+        zookeeper.create("/zk-test-ASync", "ASync".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL,
+                new AsyncCallback.StringCallback() {
+                    /**
+                     * 创建节点的异步函数
+                     * @param rc 服务端相应码
+                     * @param path
+                     * @param ctx create的最后一个传入参数 本例为 new ArrayList<>()
+                     * @param name
+                     */
+                    @Override
+                    public void processResult(int rc, String path, Object ctx, String name) {
+                        System.out.println("Create path result: [" + rc + ", " + path + ", "
+                                + ctx + ", real path name: " + name);
+                        createNodeSemaphore.countDown();
+                    }
+                }, new ArrayList<>());
+        createNodeSemaphore.await();
+    }
+
+    static class SimpleWatcher implements Watcher {
+        public void process(WatchedEvent event) {
+            if (KeeperState.SyncConnected == event.getState()) {
+                connectedSemaphore.countDown();
+            }
+        }
+    }
+}
